71ff3e039bf4504b2f61cfcd71426b9dfebb9ff5
tyVespA <migliavacca.marco08@gmail.com>
2023-02-17 10:18:39 +0100

    Set up base document template

---
+++ src/components/CvBuilder.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import s from "../styles/CvBuilder.module.css";
+
+function CvBuilder() {
+  return <section className={s.builderSection}>Name</section>;
+}
+
+export default CvBuilder;
---
+++ src/components/CvDisplayer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import s from "../styles/CvDisplayer.module.css";
+
+function CvDisplayer() {
+  return <section className={s.displayerSection}>CvDisplayer</section>;
+}
+
+export default CvDisplayer;
